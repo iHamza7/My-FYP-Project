fa7b09bef88a7988406b6f2bd9d8b1af31de1a30
Hamzx47 <khanazam3324@gmail.com>
2023-02-20 09:18:47 +0500

    lib changed

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -25,7 +25,7 @@ class _BodyState extends State<Body> {
       "image": "assets/images/pro1.jpg",
     },
     {
-      "text": "This will also help in Passport",
+      "text": "This will also help  Passport",
       "image": "assets/images/pro3.jpg",
     },
   ];
